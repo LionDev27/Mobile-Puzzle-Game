f01cbe5f3172cde23cf1ad78074db6b34aa3c3ba
LionDev27 <leonzhou2b@gmail.com>
2023-02-13 16:02:26 +0100

    Basic Path Creation with Tilemaps **BUG: NO SE AJUSTAN BIEN LAS POSICIONES SI MODIFICAMOS LA ESCALA DEL GRID**

--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -1,18 +1,57 @@
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class TileController : MonoBehaviour
 {
+    [SerializeField] private GameObject _tilePrefab;
     private Tilemap _pathMap;
-
+    
     private void Awake()
     {
         _pathMap = GetComponent<Tilemap>();
     }
 
-    private void OnMouseEnter()
+    private void Start()
     {
-        Vector3 mousePos = Input.mousePosition;
-        TileBase tile = _pathMap.GetTile(Vector3Int.FloorToInt(mousePos));
+        InitializeTiles();
+    }
+
+    private void InitializeTiles()
+    {
+        //Obtenemos los limites del tilemap.
+        BoundsInt tileBounds = _pathMap.cellBounds;
+
+        //Obtenemos la posicion de cada celda del Grid.
+        foreach (var position in tileBounds.allPositionsWithin)
+        {
+            //Si hay un tile en esa posicion.
+            if (_pathMap.HasTile(position))
+            {
+                //Lo pasamos a posicion de mundo.
+                Vector3 pos = _pathMap.CellToWorld(position);
+                pos = new Vector3(pos.x + 0.5f, pos.y + 0.5f, pos.z);
+
+                //Si modificamos la escala del Grid de alguna manera, no funciona. Hay que ver como arreglarlo.
+                #region GridScale
+
+                // //Obtenemos el tamaño de las celdas en el Grid.
+                // Vector2 cellSize = _pathMap.layoutGrid.cellSize;
+                // //Si ha cambiado el tamaño de las celdas, ajustamos la posición.
+                // if (cellSize != Vector2.one)
+                // {
+                //     // pos = new Vector3(pos.x - cellSize.x, pos.y - cellSize.y, pos.z);
+                // }
+
+                #endregion
+                
+                //Instanciamos nuestro propio tile de funcionamiento. Se puede cambiar a Object Pooling.
+                GameObject tempTile = Instantiate(_tilePrefab, pos, quaternion.identity);
+                //Cambiamos la escala del tile propio a la del Grid para que tenga el mismo tamaño.
+                tempTile.transform.localScale = _pathMap.layoutGrid.cellSize;
+            }
+        }
+        
+        //Destruimos el tilemap.
     }
 }
