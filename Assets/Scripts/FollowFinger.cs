f8e4ea0e1061342a4603e579ebdc7f4cc8876af8
Thikoon <raulilloellokillo@gmail.com>
2023-02-14 17:56:08 +0100

    Bola fix

--- Assets/Scripts/FollowFinger.cs
+++ Assets/Scripts/FollowFinger.cs
@@ -6,20 +6,25 @@ public class FollowFinger : MonoBehaviour
 {
     [SerializeField] private float _speed;
     private Rigidbody2D _rB;
+    Vector2 dir;
 
     private void Awake()
     {
         _rB = GetComponent(typeof(Rigidbody2D)) as Rigidbody2D;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
+        //Calculate
         if (Input.touchCount > 0)
         {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.touches[0].position);
-            pos.z = 0f;
-
-            _rB.position = Vector2.Lerp(transform.position, pos, _speed * Time.fixedDeltaTime);
+            dir = Camera.main.ScreenToWorldPoint(Input.touches[0].position) - transform.position;
+            dir.Normalize();
         }
+    }
+
+    private void FixedUpdate()
+    {
+        _rB.velocity = dir * _speed * Time.fixedDeltaTime;
     }
 }
